c6c2846719004c720681af328e3d74436877aef5
acnoh <andreac.noh@gmail.com>
2024-11-13 20:12:06 -0500

    added quotation marks

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ resource "azurerm_resource_group" "rg" {
 }
 
 // Virtual Network
-resource "azurerm_vnet" "vnet" {
+resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.vnet.location
-  resource_group_name = azurerm_resource_group.vnet.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   tags = local.tags
 }
@@ -44,28 +44,29 @@ resource "azurerm_vnet" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_vnet.subnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
   service_endpoints    = ["Microsoft.Sql"]
 
-  tags = local.tags
+
 }
 
 resource "azurerm_mssql_server" "sqlsrvr" {
-  name                         = "sqlsrvr-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
+  name                = "sqlsrvr-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  version             = "12.0"
+  administrator_login  = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 
   tags = local.tags
 }
 
 resource "azurerm_mssql_virtual_network_rule" "sqlsrvr_vnet" {
   name      = "sqlsrvr_vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
-  server_id = azurerm_mssql_server.sqlsrvr_vnet.id
+  server_id = azurerm_mssql_server.sqlsrvr.id
   subnet_id = azurerm_subnet.subnet.id
 
-  tags = local.tags
 }
 // Storage Account
 
