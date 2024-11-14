a672ccbe5b19984d76f36df991d73df534916f42
acnoh <andreac.noh@gmail.com>
2024-11-13 19:19:10 -0500

    modify lab files

--- main.tf
+++ main.tf
@@ -30,7 +30,43 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
+// Virtual Network
+resource "azurerm_vnet" "vnet" {
+  name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.vnet.location
+  resource_group_name = azurerm_resource_group.vnet.name
 
+  tags = local.tags
+}
+
+// Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_vnet.subnet.name
+  address_prefixes     = [10.0.2.0/24]
+  service_endpoints    = ["Microsoft.Sql"]
+
+  tags = local.tags
+}
+
+resource "azurerm_mssql_server" "sqlsrvr" {
+  name                         = "sqlsrvr-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+
+  tags = local.tags
+}
+
+resource "azurerm_mssql_virtual_network_rule" "sqlsrvr_vnet" {
+  name      = "sqlsrvr_vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  server_id = azurerm_mssql_server.sqlsrvr_vnet.id
+  subnet_id = azurerm_subnet.subnet.id
+
+  tags = local.tags
+}
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
@@ -42,3 +78,4 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
